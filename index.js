af2829e7d7fecc00a70d6e7a3b4ebf823b5fdfba
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-09 22:16:43 +0530

    Bar Chart With divs

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-d3.select('div')
-    .selectAll('p')
-    .data([1, 2, 3])
+
+const data = [
+    {id: 1, value: 10, region: 'USA'},
+    {id: 2, value: 12, region: 'India'},
+    {id: 3, value: 6, region: 'China'},
+    {id: 4, value: 9, region: 'Germany'},
+]
+
+const container = d3.select('div')
+    .classed('container', true)
+    .style('border', '1px solid red');
+
+container
+    .selectAll('.bar')
+    .data(data)
     .enter()
-    .append('p')
-    .text(data => data)
+    .append('div')
+    .classed('bar', true)
+    .style('width', '50px')
+    .style('height', data => (data.value * 10) + 'px')
+
 
