09e0b778e2d25fc40c9e6b7d9b9dff75a3e9183f
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-10 07:40:14 +0530

    Displaying simple bar chart

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const data = [
     {id: 4, value: 9, region: 'Germany'},
 ]
 
+const xScale = d3
+    .scaleBand()
+    .domain(data.map(d => d.region))
+    .rangeRound([0,250])
+    .padding(0.1);
+
+const yScale = d3
+    .scaleLinear()
+    .domain([0,15])
+    .range([300,0])
+
+
 const container = d3.select('svg')
     .classed('container', true)
     .style('border', '1px solid red');
@@ -16,7 +28,9 @@ container
     .enter()
     .append('rect')
     .classed('bar', true)
-    .attr('width', '50px')
-    .attr('height', data => (data.value * 10) + 'px')
+    .attr('width', xScale.bandwidth())
+    .attr('height', data => 300 - yScale(data.value))
+    .attr('x', data => xScale(data.region))
+    .attr('y', data => yScale(data.value))
 
 
